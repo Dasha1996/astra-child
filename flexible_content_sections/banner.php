aea72ecf29fecd4af122b431a84fabf6e3badd56
dasha-gurevichpwdpwdpwd <dashawebdev96@gmail.com>
2023-11-16 18:27:25 +0000

    e

--- flexible_content_sections/banner.php
+++ flexible_content_sections/banner.php
@@ -2,3 +2,13 @@
 $image = get_sub_field('image')["url"];
 echo $image;
 echo "test image";
+
+?><div class="banner">
+    <div class="col">
+        <img src="<?php echo $image;?>" alt =""/>
+    </div>
+    <div class="col">
+    </div>
+
+
+</div>
