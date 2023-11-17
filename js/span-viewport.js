766a4b0de0efd501ef0b0ec2a97073f235904b2f
dasha-gurevichpwdpwdpwd <dashawebdev96@gmail.com>
2023-11-17 15:16:46 +0000

    e

--- js/span-viewport.js
+++ js/span-viewport.js
@@ -1,26 +1,28 @@
-jQuery(document).ready(function() {
-    console.log("loaded");
-    $(window).on('scroll resize', function() {
-        $('.banner span').each(function() {
-            if (isElementInViewport(this)) {
-                $(this).addClass('in-view');
-            } else {
-                $(this).removeClass('in-view');
-            }
+(function($) {
+    $(document).ready(function() {
+        console.log("loaded");
+        $(window).on('scroll resize', function() {
+            $('.banner span').each(function() {
+                if (isElementInViewport(this)) {
+                    $(this).addClass('in-view');
+                } else {
+                    $(this).removeClass('in-view');
+                }
+            });
         });
+    
+        // Function to check if the element is in the viewport
+        function isElementInViewport(element) {
+            var rect = element.getBoundingClientRect();
+            return (
+                rect.top >= 0 &&
+                rect.left >= 0 &&
+                rect.bottom <= $(window).height() &&
+                rect.right <= $(window).width()
+            );
+        }
+    
+        // Trigger the scroll event initially
+        $(window).trigger('scroll');
     });
-
-    // Function to check if the element is in the viewport
-    function isElementInViewport(element) {
-        var rect = element.getBoundingClientRect();
-        return (
-            rect.top >= 0 &&
-            rect.left >= 0 &&
-            rect.bottom <= $(window).height() &&
-            rect.right <= $(window).width()
-        );
-    }
-
-    // Trigger the scroll event initially
-    $(window).trigger('scroll');
-});
+})
