8528566fe07e665d5e2c706b26dae3d2722cf784
dasha-gurevichpwdpwdpwd <dashawebdev96@gmail.com>
2023-11-18 17:37:00 +0000

    commi

--- flexible_content_sections/services.php
+++ flexible_content_sections/services.php
@@ -1,5 +1,12 @@
-test
 <?php
+if(have_rows('services')) {
+    while(have_rows('services')) {
+
+        the_row();
+        $subtitle = get_field('subtitle');
+        echo $subtitle;
+    }
+}
 //if have rows
 //while have ros
 //the row
