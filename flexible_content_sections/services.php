974b4325c86692c8523718c2074aa849e1b74cac
dasha-gurevichpwdpwdpwd <dashawebdev96@gmail.com>
2023-11-18 17:43:02 +0000

    commi

--- flexible_content_sections/services.php
+++ flexible_content_sections/services.php
@@ -1,11 +1,15 @@
 <?php
 if(have_rows('services_cards')) {
+    ?><div class="container services-container"><?php
     while(have_rows('services_cards')) {
 
         the_row();
         $title = get_sub_field('title');
-        echo $title;
+        $icon = get_sub_field('service_icon');
+        $description = get_sub_field('service_description');
+
     }
+    ?></div><?php
 }
 //if have rows
 //while have ros
