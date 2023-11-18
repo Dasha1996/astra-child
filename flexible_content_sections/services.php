6fec1f1644fa3a28d1107f1af8845fd6ead36bb8
dasha-gurevichpwdpwdpwd <dashawebdev96@gmail.com>
2023-11-18 17:47:15 +0000

    commi

--- flexible_content_sections/services.php
+++ flexible_content_sections/services.php
@@ -1,14 +1,22 @@
 <?php
 if(have_rows('services_cards')) {
-    ?><div class="container services-container"><?php
+    ?><div class="container services-container flexbox"><?php
     while(have_rows('services_cards')) {
 
         the_row();
         $title = get_sub_field('title');
         $icon = get_sub_field('service_icon');
         $description = get_sub_field('service_description');
+        ?>
+        <div class="service-card card-tab">
+            <img src="<?php echo $icon['url'];?>" alt="<?php echo $icon['alt'];?>">
+            <h3><?php echo $title;?>
+            <p><?php echo $description;?></p>
+        </div>
 
-    }
+        <?php
+
+        }
     ?></div><?php
 }
 //if have rows
