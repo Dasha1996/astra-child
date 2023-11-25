95dbcfd390fadc6fb33f68013a819e97bacd24e4
dasha-gurevichpwdpwdpwd <dashawebdev96@gmail.com>
2023-11-25 18:51:48 +0000

    commi

--- flexible_content_sections/services.php
+++ flexible_content_sections/services.php
@@ -4,7 +4,10 @@ $subtitle = get_sub_field('subtitle');
 $section_heading_intro = get_sub_field('section_heading_and_text');
 echo '<span class="center">' . $subtitle . '</span>';
 echo $section_heading_intro;
-?><img src="/wp-content/uploads/2023/11/cube.png" class="cube" alt="rotating cube with purple and pink sides"/><?
+?><img src="/wp-content/uploads/2023/11/cube.png" class="cube" alt="rotating cube with purple and pink sides"/>
+<img src="/wp-content/uploads/2023/11/arrow.png" class="arrow" alt="black dotted arrow pointing towards services"/>
+
+<?php
 if(have_rows('services_cards')) {
     ?><div class="services-container flexbox"><?php
     while(have_rows('services_cards')) {
