986eec9b27632b1d23e956d6869fc12f96f9df2a
dasha-gurevichpwdpwdpwd <dashawebdev96@gmail.com>
2023-12-16 16:55:03 +0000

    g

--- flexible_content_sections/get_in_touch.php
+++ flexible_content_sections/get_in_touch.php
@@ -1,7 +1,7 @@
 <?php
 $text = get_sub_field('text');
 $button_text = get_sub_field('button_text');
-$buttn_link = get_sub_field('button_link');
+$button_link = get_sub_field('button_link');
 ?>
 <div class="get-in-touch section">
     <div class="container">
