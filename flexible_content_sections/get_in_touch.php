d135f28b9ecdfe15f3d11060a3d7392153260199
dasha-gurevichpwdpwdpwd <dashawebdev96@gmail.com>
2023-12-16 17:00:47 +0000

    g

--- flexible_content_sections/get_in_touch.php
+++ flexible_content_sections/get_in_touch.php
@@ -6,7 +6,7 @@ $button_link = get_sub_field('button_link');
 <div class="get-in-touch section">
     <div class="container">
         <?=$text;?>
+        <a class="button" href="<?=$button_link;?>"><?=$button_text;?></a>
     </div>
     <img src="/wp-content/uploads/2023/12/cloud_transparent.png" class="absolute cloud">
-    <a class="button" href="<?=$button_link;?>"><?=$button_text;?></a>
 </div>
