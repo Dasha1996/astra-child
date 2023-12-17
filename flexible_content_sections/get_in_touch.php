68e7297dddeab64705075ba062daa958c9c14909
dasha-gurevichpwdpwdpwd <dashawebdev96@gmail.com>
2023-12-17 14:17:07 +0000

    g

--- flexible_content_sections/get_in_touch.php
+++ flexible_content_sections/get_in_touch.php
@@ -11,5 +11,5 @@ $button_link = get_sub_field('button_link');
         <a class="button" href="<?=$button_link;?>"><?=$button_text;?></a>
     </div>
     <img src="/wp-content/uploads/2023/12/cloud-3-cropped.png" class="absolute cloud cloud-right cloud-top">
-    <img src="/wp-content/uploads/2023/12/cloud-2-cropped.png" class="absolute cloud cloud-right cloud-bottom">
+    <img src="/wp-content/uploads/2023/12/cloud-4-2.png" class="absolute cloud cloud-right cloud-bottom">
 </div>
