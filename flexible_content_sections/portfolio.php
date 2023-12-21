c80256aa25effd153dc5b43f6c31d1ed1f5401b9
dasha-gurevichpwdpwdpwd <dashawebdev96@gmail.com>
2023-12-21 17:46:19 +0000

    g

--- flexible_content_sections/portfolio.php
+++ flexible_content_sections/portfolio.php
@@ -5,7 +5,7 @@ $btn_url = get_sub_field('button_url');
 if(have_rows('image_portfolio')) {
     while(have_rows('image_portfolio')) {
         the_row();
-        $image_arr = get_sub_field('image_portfolio');
+        $image_arr = get_sub_field('website_image');
         ?>
             <div class="image-wrap portfolio-container">
                 <img class="portfolio-image" src="<?php echo $image_arr['url'];?>" alt ="<?php echo $image_arr['alt'];?>">
