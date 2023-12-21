1e49207a037bb102f95921b2b8f6b8a2b1e5e896
dasha-gurevichpwdpwdpwd <dashawebdev96@gmail.com>
2023-12-21 18:30:16 +0000

    g

--- js/scroll-hover-focus.js
+++ js/scroll-hover-focus.js
@@ -1,30 +1,33 @@
-$(document).ready(function() {
-    console.log("loaded");
-    // jQuery to calculate the transition duration based on the tallest image
-    var images = $('.portfolio-section .image-wrap img');
-    var image_container = $('.portfolio-section .image-wrap');
-
-    //starting point for tallest image
-    var tallestImageHeight = 0;
-
-    images.each(function() {
-        var imageHeight = $(this).height();
-        if (imageHeight > tallestImageHeight) {
-            tallestImageHeight = imageHeight;
-        }
-    });
-
-    var transitionDuration = tallestImageHeight / 100; // Adjust as needed
-
-    // Set the custom property to apply the transition duration
-    $('.image-wrap img').css('transition', 'all' + transitionDuration + 's' + 'ease-in-out');
-
-    image_container.on('focus', function() {
-        $(this).find('img').css({
-            'object-position': 'bottom center'
+(function( $ ) { 
+    $(document).ready(function() {
+        console.log("loaded");
+        // jQuery to calculate the transition duration based on the tallest image
+        var images = $('.portfolio-section .image-wrap img');
+        var image_container = $('.portfolio-section .image-wrap');
+    
+        //starting point for tallest image
+        var tallestImageHeight = 0;
+    
+        images.each(function() {
+            var imageHeight = $(this).height();
+            if (imageHeight > tallestImageHeight) {
+                tallestImageHeight = imageHeight;
+            }
         });
+    
+        var transitionDuration = tallestImageHeight / 100; // Adjust as needed
+    
+        // Set the custom property to apply the transition duration
+        $('.image-wrap img').css('transition', 'all' + transitionDuration + 's' + 'ease-in-out');
+    
+        image_container.on('focus', function() {
+            $(this).find('img').css({
+                'object-position': 'bottom center'
+            });
+        });
+    
+    
+    
     });
 
-
-
-});
+})
