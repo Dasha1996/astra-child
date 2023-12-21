490a7525bad5e2a09cb1ea8bbbd17024cdbd7b77
dasha-gurevichpwdpwdpwd <dashawebdev96@gmail.com>
2023-12-21 18:27:57 +0000

    g

--- flexible_content_sections/portfolio.php
+++ flexible_content_sections/portfolio.php
@@ -12,7 +12,7 @@ if(have_rows('image_portfolio')) {
         the_row();
         $image_arr = get_sub_field('website_image');
         ?>
-            <div class="image-wrap portfolio-container">
+            <div class="image-wrap portfolio-container" tabindex = "0">
                 <img class="portfolio-image" src="<?php echo $image_arr['url'];?>" alt ="<?php echo $image_arr['alt'];?>">
             </div>
         <?php
