164f8cf51cec922d90e214870a5b49b1ede3701d
dasha-gurevichpwdpwdpwd <dashawebdev96@gmail.com>
2023-12-21 18:40:59 +0000

    g

--- js/scroll-hover-focus.js
+++ js/scroll-hover-focus.js
@@ -19,7 +19,7 @@
         console.log(transitionDuration);
     
         // Set the custom property to apply the transition duration
-        $('.image-wrap img').css('transition', 'all' + transitionDuration + 's ease-in-out');
+        $('.image-wrap img').css('transition', 'all ' + transitionDuration + 's ease-in-out');
     
         image_container.on('focus', function() {
             $(this).find('img').css({
