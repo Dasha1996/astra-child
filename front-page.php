3a653fb13152cfbc74cf20f7028cf76a6b47edcf
dasha-gurevichpwdpwdpwd <dashawebdev96@gmail.com>
2023-12-24 18:46:13 +0000

    g

--- front-page.php
+++ front-page.php
@@ -45,37 +45,40 @@ function astra_child_custom_content() {
 		'posts_per_page' => 3,
 	); ?>
 	<div class="testimonials-section container-custom">
-    <div id="testimonialscontrols" class="carousel slide my-4" data-ride="carousel" data-interval="8000" >
-	<div class="carousel-indicators">
-    	<button type="button" data-bs-target="#testimonialscontrols" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    	<button type="button" data-bs-target="#testimonialscontrols" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    	<button type="button" data-bs-target="#testimonialscontrols" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
-    <div class="carousel-inner">
-	<?php $blogpost = new WP_Query($args);
-	while($blogpost->have_posts()) {
-		$count++;
-	$blogpost->the_post();
-		?>
-    <div class="carousel-item <?php if($count == 1) { echo 'active'; } ?>" aria-label="Slide <?=$count;?> out of 3">
-      <p><?php the_content(); ?></p>
-      <h4 class="w-50 mx-auto mt-5" style="color:#67498f;"><?php the_title();  ?></h4>
-    </div>
-		<?php
-		}
-		  ?> 
-<button class="carousel-control-prev" type="button" data-bs-target="#testimonialscontrols" data-bs-slide="prev">
-    	<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    	<span class="visually-hidden">Previous</span>
-  	</button>
-	  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-    	<span class="carousel-control-next-icon" aria-hidden="true"></span>
-    	<span class="visually-hidden">Next</span>
-  	</button>
-	  </div>
-</div>
+		<div class="container-custom">
 
+		</div>
 	</div>
+    <div id="testimonialscontrols" class="carousel slide my-4" data-ride="carousel" data-interval="8000" >
+		<div class="carousel-indicators">
+			<button type="button" data-bs-target="#testimonialscontrols" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+			<button type="button" data-bs-target="#testimonialscontrols" data-bs-slide-to="1" aria-label="Slide 2"></button>
+			<button type="button" data-bs-target="#testimonialscontrols" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  		</div>
+   		 <div class="carousel-inner">
+			<?php $blogpost = new WP_Query($args);
+			while($blogpost->have_posts()) {
+				$count++;
+			$blogpost->the_post();
+				?>
+			<div class="carousel-item <?php if($count == 1) { echo 'active'; } ?>" aria-label="Slide <?=$count;?> out of 3">
+				<p><?php the_content(); ?></p>
+				<h4 class="w-50 mx-auto mt-5" style="color:#67498f;"><?php the_title();  ?></h4>
+			</div>
+				<?php
+				}
+				?> 
+			<button class="carousel-control-prev" type="button" data-bs-target="#testimonialscontrols" data-bs-slide="prev">
+				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+				<span class="visually-hidden">Previous</span>
+			</button>
+			<button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+				<span class="carousel-control-next-icon" aria-hidden="true"></span>
+				<span class="visually-hidden">Next</span>
+			</button>
+		</div>
+	</div>
+</div>
 
 <?php }
 ?>
