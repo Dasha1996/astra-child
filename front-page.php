85d7c691797e592c736defdeac5ea3fccb7cadd0
dasha-gurevichpwdpwdpwd <dashawebdev96@gmail.com>
2023-12-24 18:30:48 +0000

    g

--- front-page.php
+++ front-page.php
@@ -64,7 +64,6 @@ function astra_child_custom_content() {
 		<?php
 		}
 		  ?> 
-</div>
 <button class="carousel-control-prev" type="button" data-bs-target="#testimonialscontrols" data-bs-slide="prev">
     	<span class="carousel-control-prev-icon" aria-hidden="true"></span>
     	<span class="visually-hidden">Previous</span>
@@ -73,6 +72,7 @@ function astra_child_custom_content() {
     	<span class="carousel-control-next-icon" aria-hidden="true"></span>
     	<span class="visually-hidden">Next</span>
   	</button>
+	  </div>
 </div>
 
 	</div>
