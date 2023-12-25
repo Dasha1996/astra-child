c583bb411a9692b99d70557123c4adceef59d56a
dasha-gurevichpwdpwdpwd <dashawebdev96@gmail.com>
2023-12-25 19:37:48 +0000

    testimonials styling

--- template_parts/testimonials.php
+++ template_parts/testimonials.php
@@ -7,7 +7,7 @@
 	); ?>
 	<div class="testimonials-section">
 		<div class="container-custom">
-		<div id="testimonialscontrols" class="carousel slide carousel-fade" data-ride="carousel" data-interval="8000" >
+		<div id="testimonialscontrols" class="carousel slide" data-ride="carousel" data-interval="8000" >
 		<div class="carousel-indicators">
 			<button type="button" data-bs-target="#testimonialscontrols" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
 			<button type="button" data-bs-target="#testimonialscontrols" data-bs-slide-to="1" aria-label="Slide 2"></button>
