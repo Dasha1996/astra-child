11ef879c1de86d43812a1857c58040df772ce090
dasha-gurevichpwdpwdpwd <dashawebdev96@gmail.com>
2023-12-29 18:40:00 +0000

    testimonials

--- template_parts/testimonials.php
+++ template_parts/testimonials.php
@@ -7,12 +7,12 @@
 	); ?>
 	<div class="testimonials-section">
 		<div class="container-custom">
-		<div id="testimonialscontrols" class="carousel slide" data-ride="carousel" data-interval="8000" >
 		<div class="carousel-indicators">
 			<button type="button" data-bs-target="#testimonialscontrols" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
 			<button type="button" data-bs-target="#testimonialscontrols" data-bs-slide-to="1" aria-label="Slide 2"></button>
 			<button type="button" data-bs-target="#testimonialscontrols" data-bs-slide-to="2" aria-label="Slide 3"></button>
   		</div>
+		<div id="testimonialscontrols" class="carousel slide" data-ride="carousel" data-interval="8000" >
    		 <div class="carousel-inner">
 			<?php $blogpost = new WP_Query($args);
 			while($blogpost->have_posts()) {
