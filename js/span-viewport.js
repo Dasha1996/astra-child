0f6f9b6649c118a396f39171fc86dd5bd4254cee
dasha-gurevichpwdpwdpwd <dashawebdev96@gmail.com>
2024-01-02 18:12:34 +0000

    commit

--- js/span-viewport.js
+++ js/span-viewport.js
@@ -28,6 +28,9 @@
                 if (isElementInViewport(this)) {
                     $(this).addClass('in-view');
                 } 
+                else {
+                    $(this).removeClass('in-view');
+                }
             })
            
 
@@ -37,9 +40,9 @@
         function isElementInViewport(element) {
             var rect = element.getBoundingClientRect();
             return (
-                rect.top >= 0 &&
+                rect.top > 0 &&
                 rect.left >= 0 &&
-                rect.bottom <= $(window).height() &&
+                rect.bottom < $(window).height() &&
                 rect.right <= $(window).width()
             );
         }
